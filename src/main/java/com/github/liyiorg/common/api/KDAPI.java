6ffed351fb85ea3f572e47ec155c972f0f57a8ad
liyi <liyiorg@gmail.com>
2016-11-03 13:48:51 +0800

    update

--- src/main/java/com/github/liyiorg/common/api/KDAPI.java
+++ src/main/java/com/github/liyiorg/common/api/KDAPI.java
@@ -24,119 +24,186 @@ import com.github.liyiorg.common.result.Kuaidi;
  *
  *
  快递公司简称
-A
+
+
+分类 	快递公司代码 	公司名称 
+A		
+	auspost	澳大利亚邮政(英文结果）
 	aae	AAE
 	anxindakuaixi	安信达
-B
+B		
+	huitongkuaidi	百世汇通
 	baifudongfang	百福东方
 	bht	BHT
-	youzhengguonei	包裹/平邮/挂号信（暂不支持，请转用Open_API_APICode_URL）
-C
+	youzhengguonei	包裹/平邮/挂号信（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
+	bangsongwuliu	邦送物流
+C		
 	cces	希伊艾斯（CCES）
 	coe	中国东方（COE）
-D
+	chuanxiwuliu	传喜物流
+	canpost	加拿大邮政Canada Post（英文结果）
+	canpostfr	加拿大邮政Canada Post(德文结果）
+D		
 	datianwuliu	大田物流
 	debangwuliu	德邦物流
 	dpex	DPEX
-	dhl	DHL
+	dhl	DHL-中国件-中文结果
+	dhlen	DHL-国际件-英文结果
+	dhlde	DHL-德国件-德文结果（德国国内派、收的件）
 	dsukuaidi	D速快递
-E
-	ems	EMS
+	disifang	递四方
+E		
+	ems	EMS(中文结果)
+ （暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
 	ems	E邮宝
-F
-	fedex	Fedex（国际）（说明：Fedex是国际，国内的请用“lianbangkuaidi”）
+（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
+	emsen	EMS（英文结果）
+（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
+	emsguoji	EMS-（中国-国际）件-中文结果/EMS-(China-International）-Chinese data
+（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
+	emsinten	EMS-（中国-国际）件-英文结果/EMS-(China-International）-Englilsh data
+（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
+F		
+	fedex	Fedex-国际件-英文结果（说明：Fedex是国际件的英文结果，Fedex中国的请用“lianbangkuaidi”，Fedex-美国请用fedexus）
+	fedexcn	Fedex-国际件-中文结果
+	fedexus	Fedex-美国件-英文结果(说明：如果无效，请偿试使用fedex）
 	feikangda	飞康达物流
 	feikuaida	飞快达
 	rufengda	凡客如风达
-G
+	fengxingtianxia	风行天下
+	feibaokuaidi	飞豹快递
+G		
 	ganzhongnengda	港中能达
+	guotongkuaidi	国通快递
 	guangdongyouzhengwuliu	广东邮政
-	youzhengguonei	挂号信（暂不支持，请转用Open_API_APICode_URL）
-	youzhengguonei	国内邮件（暂不支持，请转用Open_API_APICode_URL）
-	youzhengguoji	国际邮件（暂不支持，请转用Open_API_APICode_URL）
+	youzhengguonei	挂号信
+（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
+	youzhengguonei	国内邮件
+（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
+	youzhengguoji	国际邮件
+（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
 	gls	GLS
-H
+	gongsuda	共速达
+H		
 	huitongkuaidi	汇通快运
+	huiqiangkuaidi	汇强快递
+	tiandihuayu	华宇物流
 	hengluwuliu	恒路物流
 	huaxialongwuliu	华夏龙
 	tiantian	海航天天
 	haiwaihuanqiu	海外环球
-J
-	jinguangsudikuaijian	京广速递
-	jixianda	急先达
+	hebeijianhua	河北建华（暂只能查好乐买的单，其他商家要查，请发邮件至 wensheng_chen#kingdee.com(将#替换成@)开通权限
+	haimengsudi	海盟速递
+	huaqikuaiyun	华企快运
+	haihongwangsong	山东海红
+J		
 	jiajiwuliu	佳吉物流
 	jiayiwuliu	佳怡物流
 	jiayunmeiwuliu	加运美
+	jinguangsudikuaijian	京广速递
+	jixianda	急先达
 	jinyuekuaidi	晋越快递
-K
+	jietekuaidi	捷特快递
+	jindawuliu	金大物流
+	jialidatong	嘉里大通
+K		
 	kuaijiesudi	快捷速递
-L
+	kangliwuliu	康力物流
+	kuayue	跨越物流
+L		
 	lianhaowuliu	联昊通
 	longbanwuliu	龙邦物流
 	lanbiaokuaidi	蓝镖快递
-	lejiedi	乐捷递（暂不支持）
-	lianbangkuaidi	联邦快递（国内）（说明：国外的请用 fedex）
-M
-	minghangkuaidi	民航快递
-R
-	rufengda	如风达快递
-Q
+	lejiedi	乐捷递（暂只能查好乐买的单，其他商家要查，请发邮件至 wensheng_chen#kingdee.com(将#替换成@)开通权限
+	lianbangkuaidi	联邦快递（Fedex-中国-中文结果）（说明：国外的请用 fedex）
+	lianbangkuaidien	联邦快递(Fedex-中国-英文结果）
+	lijisong	立即送（暂只能查好乐买的单，其他商家要查，请发邮件至 wensheng_chen#kingdee.com(将#替换成@)开通权限)
+	longlangkuaidi	隆浪快递
+M		
+	menduimen	门对门
+	meiguokuaidi	美国快递
+	mingliangwuliu	明亮物流
+O		
+	ocs	OCS
+	ontrac	onTrac
+Q		
 	quanchenkuaidi	全晨快递
 	quanjitong	全际通
 	quanritongkuaidi	全日通
 	quanyikuaidi	全一快递
 	quanfengkuaidi	全峰快递
-S
-	shentong	申通速递
-	shentong	申通E物流
-	shunfeng	顺丰速递
+	sevendays	七天连锁
+R		
+	rufengda	如风达快递
+S		
+	shentong	申通（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
+	shunfeng	顺丰（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
+	shunfengen	顺丰（英文结果）（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
 	santaisudi	三态速递
 	shenghuiwuliu	盛辉物流
 	suer	速尔物流
 	shengfengwuliu	盛丰物流
 	shangda	上大物流
 	santaisudi	三态速递
-	shandonghaihong	山东海红
-	saiodi	赛澳递
-T
+	haihongwangsong	山东海红
+	saiaodi	赛澳递
+	haihongwangsong	山东海红（暂只能查好乐买的单，其他商家要查，请发邮件至 wensheng_chen#kingdee.com(将#替换成@)开通权限）
+	sxhongmajia	山西红马甲（暂只能查天天网的单，其他商家要查，请发邮件至 wensheng_chen#kingdee.com(将#替换成@)开通权限)
+	shenganwuliu	圣安物流
+	suijiawuliu	穗佳物流
+T		
 	tiandihuayu	天地华宇
 	tiantian	天天快递
-	tnt	TNT
-U
-	ups	UPS
-W
+	tnt	TNT（中文结果）
+	tnten	TNT（英文结果）
+	tonghetianxia	通和天下（暂只能查好乐买的单，其他商家要查，请发邮件至 wensheng_chen#kingdee.com(将#替换成@)开通权限）
+U		
+	ups	UPS（中文结果）
+	upsen	UPS（英文结果）
+	youshuwuliu	优速物流
+	usps	USPS（中英文）
+W		
 	wanjiawuliu	万家物流
-	wxwl	万象物流（即将支持）
-X
+	wanxiangwuliu	万象物流
+	weitepai	微特派（暂只能查天天网的单，其他商家要查，请发邮件至 wensheng_chen#kingdee.com(将#替换成@)开通权限)
+X		
 	xinbangwuliu	新邦物流
 	xinfengwuliu	信丰物流
-	xingchengjibian	星晨急便（暂不支持，请转用Open_API_APICode_URL）
-	xinhongyukuaidi	鑫飞鸿
-Y
+	xingchengjibian	星晨急便（暂不支持，该公司已不存在）
+	xinhongyukuaidi	鑫飞鸿（暂不支持，该公司已不存在）
+	cces	希伊艾斯(CCES)（暂不支持，该公司已不存在）
+	xinbangwuliu	新邦物流
+	neweggozzo	新蛋奥硕物流
+	hkpost	香港邮政
+Y		
+	yuantong	圆通速递（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
+	yunda	韵达快运（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
+	yuntongkuaidi	运通快递
+	youzhengguonei	邮政小包（国内），邮政包裹（国内）、邮政国内给据（国内）（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
+	youzhengguoji	邮政小包（国际），邮政包裹（国际）、邮政国内给据（国际）（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
+	yuanchengwuliu	远成物流
 	yafengsudi	亚风速递
 	yibangwuliu	一邦速递
 	youshuwuliu	优速物流
-	yuanchengwuliu	远成物流
-	yuantong	圆通速递
 	yuanweifeng	源伟丰快递
 	yuanzhijiecheng	元智捷诚
 	yuefengwuliu	越丰物流
-	yunda	韵达快运
 	yuananda	源安达
 	yuanfeihangwuliu	原飞航
-	yuntongkuaidi	运通快递
 	zhongxinda	忠信达快递
 	zhimakaimen	芝麻开门
-	youzhengguonei	邮政国内给据（暂不支持，请转用Open_API_APICode_URL）
-	youzhengguoji	邮政国际（暂不支持，请转用Open_API_APICode_URL）
-Z
+	yinjiesudi	银捷速递
+	yitongfeihong	一统飞鸿（暂只能查天天网的单，其他商家要查，请发邮件至 wensheng_chen#kingdee.com(将#替换成@)开通权限)
+Z		
+	zhongtong	中通速递（暂只支持HtmlAPI,要JSON、XML格式结果和签收状态state请转用【企业版】，
 	zhaijisong	宅急送
-	zhongtiewuliu	中铁快运
-	zhongtong	中通速递
 	zhongyouwuliu	中邮物流
-
- *
- */
+	zhongxinda	忠信达
+	zhongsukuaidi	中速快件
+	zhimakaimen	芝麻开门
+	zhengzhoujianhua	郑州建华（暂只能查好乐买的单，其他商家要查，请发邮件至 wensheng_chen#kingdee.com(将#替换成@)开通权限）
+	zhongtianwanyun	中天万运
+*/
 public class KDAPI extends BaseAPI{
 	
 	private static String URI = "h t t p s : // w w w . k u a i d i 1 0 0 . c o m".replace(" ", "");
